042b434abf6b46b9079a2984cd3894b3c79d61bf
mounir <marnissimounir05@gmail.com>
2024-01-26 16:18:33 +0100

    building container

--- webapp/src/Components/Container.jsx
+++ webapp/src/Components/Container.jsx
@@ -1,7 +1,7 @@
 import SearchBar from "./SearchBar";
 export default function Container() {
   return (
-    <div className="bg-red-500 w-[95%] mr-4 my-4">
+    <div className="bg-blue-500 w-[95%] mr-4 my-4">
       <SearchBar />
     </div>
   );
--- webapp/src/Components/SearchBar.jsx
+++ webapp/src/Components/SearchBar.jsx
@@ -1,10 +1,18 @@
 import { CgSearch } from "react-icons/cg";
 export default function SearchBar() {
   return (
-    <div className="flex items-center justify-between ">
-      <CgSearch />
-      <input type="search" name="search" />
-      <button>Search</button>
+    <div className="flex items-center justify-between bg-red-500">
+      <CgSearch className="w-[30px] h-[30px] m-2" />
+      <input
+        className="w-full bg-transparent border-none outline-none px-4 py-2 placeholder:text-gray-300"
+        type="search"
+        name="search"
+        placeholder="Search for Movies and tv-shows"
+      />
+
+      <button className="inline-block py-2 px-6 rounded-lg bg-red-500 hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200 text-gray-50 font-bold leading-loose transition duration-200 m-2">
+        Search
+      </button>
     </div>
   );
 }
