7aa9fea4f2e5630459310cd37e604914e2c82274
mounir <marnissimounir05@gmail.com>
2024-01-30 18:49:26 +0100

    redesing search container

--- webapp/src/Components/1.container/SearchContainer.jsx
+++ webapp/src/Components/1.container/SearchContainer.jsx
@@ -5,6 +5,7 @@ import { IoArrowBackOutline } from "react-icons/io5";
 import { MdLocalMovies } from "react-icons/md";
 import { PiTelevisionSimpleFill } from "react-icons/pi";
 import { LuDot } from "react-icons/lu";
+import { useState } from "react";
 export default function SearchContainer({
   query,
   onClose,
@@ -18,11 +19,17 @@ export default function SearchContainer({
     onClose();
     setQuery("");
   };
+  // let TotalPages = AllData.total_pages;
+  let TotalPages = 5;
+  const [CurrentPage, setCurrentPage] = useState(1);
   const handleNextPage = () => {
     setPageNum(PageNum + 1);
+    setCurrentPage(CurrentPage + 1);
   };
+
   const handlePrevPage = () => {
     setPageNum(PageNum - 1);
+    setCurrentPage(CurrentPage - 1);
   };
 
   return (
@@ -35,8 +42,8 @@ export default function SearchContainer({
           <FaTimes />
         </button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-center">
-      {/* <div className="grid grid-cols-4 justufy-center"> */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 justify-center">
+        {/* <div className="grid grid-cols-4 justufy-center"> */}
         {Array.isArray(Data) ? (
           Data.filter((item) => item.backdrop_path).map((item) => (
             <div key={item.id} className="mb-6">
@@ -71,7 +78,7 @@ export default function SearchContainer({
           <p>Data is not an array</p>
         )}
       </div>
-      <div className="flex justify-center gap-[1rem]">
+      <div className="flex justify-center gap-[1rem] mb-4">
         <button
           className={` ${
             PageNum <= 1 ? " bg-blue-300" : "bg-blue-500"
@@ -82,11 +89,14 @@ export default function SearchContainer({
           <IoArrowBackOutline />
         </button>
         <div className="bg-blue-500 px-4 rounded flex justify-center items-center tracking-wide">
-          {AllData.total_pages}
+          {CurrentPage} / {TotalPages}
         </div>
         <button
-          className="bg-blue-500  text-white font-bold py-2 px-4 rounded flex justify-center gap-[1rem] items-center"
+          className={` ${
+            CurrentPage === TotalPages ? " bg-blue-300" : "bg-blue-500"
+          }bg-blue-500  text-white font-bold py-2 px-4 rounded flex justify-center gap-[1rem] items-center`}
           onClick={handleNextPage}
+          disabled={CurrentPage === TotalPages}
         >
           <IoArrowForwardOutline />
         </button>
