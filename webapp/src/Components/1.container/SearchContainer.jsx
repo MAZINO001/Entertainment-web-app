d4d26dae83cc39b5c31c80838fa34138f72f6040
mounir <marnissimounir05@gmail.com>
2024-01-30 18:14:58 +0100

    working on api

--- webapp/src/Components/1.container/SearchContainer.jsx
+++ webapp/src/Components/1.container/SearchContainer.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 import { FaTimes } from "react-icons/fa"; // Corrected import
-
+import { IoArrowForwardOutline } from "react-icons/io5";
+import { IoArrowBackOutline } from "react-icons/io5";
+import { MdLocalMovies } from "react-icons/md";
+import { PiTelevisionSimpleFill } from "react-icons/pi";
+import { LuDot } from "react-icons/lu";
 export default function SearchContainer({
   query,
   onClose,
@@ -14,7 +18,6 @@ export default function SearchContainer({
     onClose();
     setQuery("");
   };
-
   const handleNextPage = () => {
     setPageNum(PageNum + 1);
   };
@@ -29,10 +32,11 @@ export default function SearchContainer({
           Found {AllData.total_results} results for {`'${query}'`}
         </h2>
         <button onClick={handleContainerClose} className="text-lg">
-          <FaTimes /> {/* Corrected icon component */}
+          <FaTimes />
         </button>
       </div>
-      <div className="grid grid-cols-4 justufy-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-center">
+      {/* <div className="grid grid-cols-4 justufy-center"> */}
         {Array.isArray(Data) ? (
           Data.filter((item) => item.backdrop_path).map((item) => (
             <div key={item.id} className="mb-6">
@@ -41,23 +45,52 @@ export default function SearchContainer({
                 src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
                 alt="Poster"
               />
-              <p>
-                <span>{item.media_type} : </span>
+              <p className="text-sm py-1 capitalize text-gray-300 flex items-center ">
                 <span>
                   {item.media_type === "tv"
                     ? item.first_air_date
                     : item.release_date}
                 </span>
+
+                <LuDot className="text-xl text-gray-300 " />
+                <span className="flex items-center gap-1">
+                  {item.media_type === "tv" ? (
+                    <PiTelevisionSimpleFill />
+                  ) : (
+                    <MdLocalMovies />
+                  )}
+                  {item.media_type}
+                </span>
               </p>
-              <h3>{item.media_type === "tv" ? item.name : item.title}</h3>
+              <h2 className="text-xl font-semibold ">
+                {item.media_type === "tv" ? item.name : item.title}
+              </h2>
             </div>
           ))
         ) : (
           <p>Data is not an array</p>
         )}
       </div>
-      <button onClick={handlePrevPage}>PrevPage</button>
-      <button onClick={handleNextPage}>NextPage</button>
+      <div className="flex justify-center gap-[1rem]">
+        <button
+          className={` ${
+            PageNum <= 1 ? " bg-blue-300" : "bg-blue-500"
+          }  text-white font-bold py-2 px-4 rounded flex justify-center gap-[1rem] items-center`}
+          onClick={handlePrevPage}
+          disabled={PageNum <= 1}
+        >
+          <IoArrowBackOutline />
+        </button>
+        <div className="bg-blue-500 px-4 rounded flex justify-center items-center tracking-wide">
+          {AllData.total_pages}
+        </div>
+        <button
+          className="bg-blue-500  text-white font-bold py-2 px-4 rounded flex justify-center gap-[1rem] items-center"
+          onClick={handleNextPage}
+        >
+          <IoArrowForwardOutline />
+        </button>
+      </div>
     </div>
   );
 }
