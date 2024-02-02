bf3488a9da78d5c29b0540037ed1b861303b7beb
mounir <marnissimounir05@gmail.com>
2024-02-02 14:57:44 +0100

    update routers

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,8 +1,40 @@
-import Wrapper from "./Components/wrapper";
+// import Wrapper from "./Components/wrapper";
+// export default function App() {
+//   return (
+//     <div className="text-white w-full h-full">
+//       <Wrapper />
+//     </div>
+//   );
+// }
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Movies from "./Components/2.SideBar/Movies";
+import TvShows from "./Components/2.SideBar/TvShows";
+import Library from "./Components/2.SideBar/Library";
+import Home from "./Components/1.container/Home";
+import NotFound from "./NotFound";
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/movies",
+      element: <Movies />,
+    },
+    {
+      path: "/tvshows",
+      element: <TvShows />,
+    },
+    {
+      path: "/libraray",
+      element: <Library />,
+    },
+  ]);
   return (
     <div className="text-white w-full h-full">
-      <Wrapper />
+      <RouterProvider router={router} />
     </div>
   );
 }
