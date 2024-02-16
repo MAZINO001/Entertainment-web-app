72b055282756f778b3ecccc81596d34bc74afb89
mounir <marnissimounir05@gmail.com>
2024-02-16 19:43:25 +0100

    working on library

--- webapp/src/CustomeHooks/useLocalStorage.jsx
+++ webapp/src/CustomeHooks/useLocalStorage.jsx
@@ -7,7 +7,7 @@ function useLocalStorage(key, initialValue) {
 
   const updateValue = (newValue) => {
     setValue(newValue);
-    localStorage.setItem(key, newValue);
+    localStorage.setItem(key, JSON.stringify(...newValue);
   };
 
   return [value, updateValue];
