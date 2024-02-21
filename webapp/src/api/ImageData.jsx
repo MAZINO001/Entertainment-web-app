1a0170e611dbdf6370d4f748feb48080032a16f1
mounir <marnissimounir05@gmail.com>
2024-02-21 14:16:24 +0100

    working on sidebar

---
+++ webapp/src/api/ImageData.jsx
@@ -0,0 +1,31 @@
+export const fetchImageData = async (Id) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzg5MjE1MDdlZjBjNjdlNTNhNjc3OTM2NGU0NjBhZSIsInN1YiI6IjY1YjY1ZWY2MmZhZjRkMDE3Y2RkYjAzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OcvYLoz0Ugh1SREfo1q2zt1xDPQ7U7O9e9tdPNbxaok",
+    },
+  };
+
+//   const url = `https://api.themoviedb.org/3/movie/${Id}`;
+
+  if (!url) {
+    throw new Error(`Invalid data Id: ${Id}`);
+  }
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    console.log(data.results);
+    //   return data.results;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch data");
+  }
+};
