cf5a039c94fedc131cdfc362fae61dc956987f1f
mounir <marnissimounir05@gmail.com>
2024-02-21 22:39:01 +0100

    working on image

--- webapp/src/Components/1.container/ImageContainer.jsx
+++ webapp/src/Components/1.container/ImageContainer.jsx
@@ -1,43 +1,10 @@
-// import { useLocation } from "react-router-dom";
-
-// export default function ImageContainer() {
-//   const location = useLocation();
-//   const passedData = location.state?.data;
-//     console.log(passedData);
-//   return (
-//     <div>
-//       {passedData
-//         .filter((item) => item.backdrop_path)
-//         .map((item) => {
-//           <div key={item.id}>
-//             <div>
-//               <img
-//                 className=" rounded-md cursor-pointer   "
-//                 src={`https://image.tmdb.org/t/p/original/${item.poster_pat}`}
-//                 alt="Poster"
-//               />
-//             </div>
-//             <div>
-//               <h2>{item.title}</h2>
-//               <span>{item.tagline}</span>
-//               <h2>{item.vote_average}</h2>
-//             </div>
-//           </div>;
-//         })}
-//     </div>
-//   );
-// }
-
 import { useLocation } from "react-router-dom";
-
 export default function ImageContainer() {
   const location = useLocation();
   const passedData = location.state?.data;
-  console.log(passedData);
-
   return (
     <div className="flex gap-x-4 ">
-      <div className="w-[40%] ">
+      <div className="w-[40%]">
         <img
           className="rounded-md cursor-pointer "
           src={`https://image.tmdb.org/t/p/original/${passedData.poster_path}`}
@@ -45,42 +12,55 @@ export default function ImageContainer() {
         />
       </div>
       <div className="w-[60%]">
-        <h2 className="text-4xl ">{passedData.title}</h2>
-        <span className="text-sm text-gray-100">{passedData.tagline} </span>
-        <h2 className="text-2xl">{passedData.vote_average} </h2>
-        <div className="flex justify-between gap-8 text-lg">
-          <div className="flex flex-col text-center ">
+        {/* title */}
+        <div className="mb-5 ">
+          <h2 className="text-4xl ">{passedData.title}</h2>
+          <span className="text-sm text-gray-400">{passedData.tagline} </span>
+        </div>
+        {/* rate */}
+        <div className="mb-3">
+          <h2 className="text-3xl">{passedData.vote_average} </h2>
+        </div>
+        {/* info */}
+        <div className="flex justify-  text-lg font-bold tracking-wide mb-4">
+          <div className="flex flex-col text-center">
             <h2>Length</h2>
-            <h2>{passedData.runtime}</h2>
+            <h2 className="text-gray-400">{passedData.runtime} mn</h2>
           </div>
-          <div className="flex flex-col text-center ">
+          <div className="flex flex-col text-center">
             <h2>Language</h2>
-            <h2>{passedData.spoken_languages[0].english_name}</h2>
+            <h2 className="text-gray-400">
+              {passedData.spoken_languages[0].english_name}
+            </h2>
           </div>
-          <div className="flex flex-col text-center ">
+          <div className="flex flex-col text-center">
             <h2>Year</h2>
-            <h2>{passedData.release_date}</h2>
+            <h2 className="text-gray-400">
+              {new Date(passedData.release_date).getFullYear()}
+            </h2>
           </div>
-          <div className="flex flex-col text-center ">
+          <div className="flex flex-col text-center">
             <h2>Status</h2>
-            <h2>{passedData.status}</h2>
+            <h2 className="text-gray-400">{passedData.status}</h2>
           </div>
         </div>
-        <div >
-          <h2 className="text-xl">Genres</h2>
+        {/* genres */}
+        <div className="mb-4">
+          <h2 className="text-xl mb-2 ">Genres</h2>
           {passedData.genres.map((item) => (
             <div key={item.id} className="flex">
-              <p className="bg-white text-[#10141E] px-2 py-1 rounded-md ">
+              <p className="bg-white text-[#10141E] px-2 py-1 rounded-md font-semibold">
                 {item.name}
               </p>
             </div>
           ))}
         </div>
+        {/* OverView */}
         <div className="">
-          <h2 className="text-xl">OverView</h2>
-          <p>{passedData.overview}</p>
+          <h2 className="text-xl mb-2">OverView</h2>
+          <p className="mb-4 text-md text-gray-400">{passedData.overview}</p>
         </div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 font-semibold">
           <div className="bg-white text-[#10141E] px-2 py-1 rounded-md ">
             <a
               href={`https://www.imdb.com/title/${passedData.imdb_id}/`}
@@ -100,7 +80,11 @@ export default function ImageContainer() {
             </a>
           </div>
           <div className="bg-white text-[#10141E] px-2 py-1 rounded-md ">
-            <a href={``} target="_blank" rel="noopener noreferrer">
+            <a
+              href={`${passedData.homepage}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Website
             </a>
           </div>
