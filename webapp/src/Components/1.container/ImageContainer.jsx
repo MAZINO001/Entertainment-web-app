62d7e5d869d60b1b6c68390c89da09b3bd63a5eb
mounir <marnissimounir05@gmail.com>
2024-02-24 14:53:52 +0100

    working on image

--- webapp/src/Components/1.container/ImageContainer.jsx
+++ webapp/src/Components/1.container/ImageContainer.jsx
@@ -12,31 +12,31 @@ export default function ImageContainer() {
   }
 
   return (
-    <div className=" flex md:flex-row flex-col gap-x-8 m-4 sm:m-0">
-      <div className="w-[35%]">
+    <div className=" flex md:flex-row flex-col gap-x-8 m-4 sm:m-0 ">
+      <div className=" w-[300px] mx-auto md:w-[40%] ">
         <img
           className="rounded-md cursor-pointer flex items-center justify-center mb-4 "
           src={`https://image.tmdb.org/t/p/original/${passedData.poster_path}`}
           alt="Poster"
         />
       </div>
-      <div className="flex items-center justify-center flex-col">
+      <div className="flex flex-col text-center md:text-left md:w-[60%]">
         {/* title */}
-        <div className="mb-5 ">
+        <div className="mb-5">
           <h2 className="text-5xl mb-2">{passedData.title}</h2>
           <span className="text-md text-gray-400 ">{passedData.tagline} </span>
         </div>
         {/* rate */}
-        <div className="mb-3">
+        <div className="mb-4 ">
           <h2 className="text-3xl inline-flex ">
             {(passedData.vote_average / 2).toFixed(1)}{" "}
           </h2>
         </div>
         {/* info */}
-        <div className="flex justify-start gap-x-20  text-lg font-bold tracking-wide mb-4">
+        <div className="flex justify-evenly  text-lg font-semibold tracking-wide mb-4">
           <div className="flex flex-col text-center">
             <h2>Length</h2>
-            <h2 className="text-gray-400">{passedData.runtime} mn</h2>
+            <h2 className="text-gray-400">{passedData.runtime}</h2>
           </div>
           <div className="flex flex-col text-center">
             <h2>Language</h2>
@@ -60,7 +60,7 @@ export default function ImageContainer() {
           <h2 className="text-xl mb-2 ">Genres</h2>
           {passedData.genres.map((item) => (
             <div key={item.id} className="inline-flex ">
-              <p className="bg-white text-[#10141E] px-3 mr-4 py-2  rounded-md font-semibold">
+              <p className="bg-white text-[#10141E] px-3 m-2 py-2  rounded-md font-semibold">
                 {item.name}
               </p>
             </div>
@@ -69,10 +69,10 @@ export default function ImageContainer() {
         {/* OverView */}
         <div className="">
           <h2 className="text-xl mb-2">OverView</h2>
-          <p className="mb-4 text-md text-gray-400">{passedData.overview}</p>
+          <p className="mb-4 text-md  text-gray-400">{passedData.overview}</p>
         </div>
         {/* buttons */}
-        <div className="flex gap-4 tracking-wide">
+        <div className="flex justify-center gap-4 tracking-wide">
           <div className="bg-[#5A6A90] text-[#fff] px-4 py-2 rounded-md hover:bg-red-500 duration-300 ">
             <a
               href={`https://www.imdb.com/title/${passedData.imdb_id}/`}
