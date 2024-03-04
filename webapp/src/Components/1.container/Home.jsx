d68b1df0c4740005513ebeb11c08ae148e6d0c5c
mounir <marnissimounir05@gmail.com>
2024-03-04 15:52:43 +0100

    fix performence

--- webapp/src/Components/1.container/Home.jsx
+++ webapp/src/Components/1.container/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
       <TrendingMovies />
       <PopularMovies />
       <NowPlayingMovies />
+      
       <UpCommingMovies />
       <TopRatedMovies />
       <hr className="my-4" />
