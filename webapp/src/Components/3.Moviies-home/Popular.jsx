8e5675fbeffabd09bbbf3b3512d5f0c08fc8fb03
mounir <marnissimounir05@gmail.com>
2024-03-04 18:04:50 +0100

    adding lazy loading

--- webapp/src/Components/3.Moviies-home/Popular.jsx
+++ webapp/src/Components/3.Moviies-home/Popular.jsx
@@ -1,3 +1,96 @@
+// /* eslint-disable react-hooks/rules-of-hooks */
+// /* eslint-disable no-unused-vars */
+// import { NavLink } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { fetchTMDbDataMovies } from "../../api/FetchDataMovie";
+// import { MdLocalMovies } from "react-icons/md";
+// import { BsBookmarkPlusFill, BsBookmarkCheckFill } from "react-icons/bs";
+// import { LuDot } from "react-icons/lu";
+// import { useQuery } from "@tanstack/react-query";
+// import Loader from "../../Loaders/Loader";
+// import { useBookmarks } from "../../CustomeHooks/useLocalStorage";
+
+// export default function Popular() {
+//   const { bookmarkedMovies, addMovieBookmark, removeMovieBookmark } =
+//     useBookmarks();
+//   const [ActiveBm, setActiveBm] = useState(false);
+//   const [type, settype] = useState("Movies");
+//   const [query, setquery] = useState("Popular");
+//   const {
+//     data: Popular,
+//     isLoading,
+//     error,
+//   } = useQuery({
+//     queryKey: ["Popular"],
+//     queryFn: () => fetchTMDbDataMovies("popular"),
+//   });
+
+//   if (isLoading) {
+//     return <Loader />;
+//   }
+//   if (error) {
+//     return <h2>{error.message}</h2>;
+//   }
+
+//   return (
+//     <div className=" flex flex-col my-4">
+//       <div className="flex justify-between items-center relative ">
+//         <h2 className="title">Popular</h2>
+//         <span className="typespan ml-[-15px]">Movies</span>
+//         <NavLink
+//           to={`seeallcontainer/${type}/${query}`}
+//           state={{ data: Popular }}
+//           className="text-xl sm:text-lg text-[#FC4747] px-2 py-1 rounded-md"
+//         >
+//           see all
+//         </NavLink>
+//       </div>
+//       <div className="  grid grid-cols-1 sm:grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 pt-4  md:pt-0 ">
+//         {Popular.filter((item) => item.backdrop_path)
+//           .slice(0, 8)
+//           .map((item) => (
+//             <div key={item.id} className="relative">
+//               {bookmarkedMovies.includes(item.id) ? (
+//                 <BsBookmarkCheckFill
+//                   className="absolute top-0 right-[-3px] cursor-pointer text-2xl text-[#FC4747]"
+//                   onClick={() => {
+//                     setActiveBm((state) => !state);
+//                     removeMovieBookmark(item.id); // Remove bookmark
+//                   }}
+//                 />
+//               ) : (
+//                 <BsBookmarkPlusFill
+//                   className="absolute top-0 right-[-3px] cursor-pointer text-2xl "
+//                   onClick={() => {
+//                     setActiveBm((state) => !state);
+//                     addMovieBookmark(item.id); // Add bookmark
+//                   }}
+//                 />
+//               )}
+//               <NavLink to={`/imagecontainer/${type}/${item.id}`}>
+//                 <img
+//                   className="rounded-md cursor-pointer"
+//                   src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+//                   alt="Poster"
+//                 />
+//               </NavLink>
+//               <p className=" text-md py-1 capitalize text-gray-300 flex items-center text-slim ">
+//                 <span>{new Date(item.release_date).getFullYear()}</span>
+
+//                 <LuDot className="text-xl text-gray-300 " />
+//                 <span className="flex items-center gap-x-1">
+//                   <MdLocalMovies /> Movie
+//                 </span>
+//               </p>
+//               <h2 className="text-xl font-semibold max-w-[100%]">
+//                 {item.title}
+//               </h2>
+//             </div>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
 import { NavLink } from "react-router-dom";
--- webapp/src/Components/3.Moviies-home/Trending.jsx
+++ webapp/src/Components/3.Moviies-home/Trending.jsx
@@ -1,3 +1,99 @@
+// /* eslint-disable no-unused-vars */
+// import { fetchTMDbDataMovies } from "../../api/FetchDataMovie";
+// import { MdLocalMovies } from "react-icons/md";
+// import { LuDot } from "react-icons/lu";
+// import { useQuery } from "@tanstack/react-query";
+// import Loader from "../../Loaders/Loader";
+// import { BsBookmarkCheckFill, BsBookmarkPlusFill } from "react-icons/bs";
+// import { useEffect, useState } from "react";
+// import useLocalStorage, {
+//   useBookmarks,
+// } from "../../CustomeHooks/useLocalStorage";
+// import { NavLink } from "react-router-dom";
+// export default function Trending() {
+//   const { bookmarkedMovies, addMovieBookmark, removeMovieBookmark } = useBookmarks();
+
+//   const [ActiveBm, setActiveBm] = useState(false);
+//   const [type, settype] = useState("Movies");
+//   const [query, setquery] = useState("Trending");
+
+//   const {
+//     data: Trending,
+//     isLoading,
+//     error,
+//   } = useQuery({
+//     queryKey: ["Trending"],
+//     queryFn: () => fetchTMDbDataMovies("trending"),
+//   });
+
+//   if (isLoading) {
+//     return <Loader />;
+//   }
+//   if (error) {
+//     return <h2>{error.message}</h2>;
+//   }
+//   return (
+//     <div className=" flex flex-col">
+//       <div className="flex justify-between items-center relative">
+//         <h2 className="title">Trending</h2>
+//         <span className="typespan">Movies</span>
+//         <NavLink
+//           to={`seeallcontainer/${type}/${query}`}
+//           state={{ data: Trending }}
+//           className="text-xl sm:text-lg text-[#FC4747] px-2 py-1 rounded-md"
+//         >
+//           see all
+//         </NavLink>
+//       </div>
+//       <div className=" grid grid-cols-[repeat(10,_1fr)] grid-rows-[1fr] gap-x-4 overflow-x-auto">
+//         {" "}
+//         {Trending.filter((item) => item.backdrop_path)
+//           .slice(0, 10)
+//           .map((item) => (
+//             <div key={item.id} className="w-[407px] relative ">
+//               {bookmarkedMovies.includes(item.id) ? (
+//                 <BsBookmarkCheckFill
+//                   className="absolute top-0 right-[-3px] cursor-pointer text-2xl text-[#FC4747] z-50"
+//                   onClick={() => {
+//                     setActiveBm((state) => !state);
+//                     removeMovieBookmark(item.id); // Remove bookmark
+//                   }}
+//                 />
+//               ) : (
+//                 <BsBookmarkPlusFill
+//                   className="absolute top-0 right-[-3px] cursor-pointer text-2xl z-50"
+//                   onClick={() => {
+//                     setActiveBm((state) => !state);
+//                     addMovieBookmark(item.id); // Add bookmark
+//                   }}
+//                 />
+//               )}
+//               <NavLink to={`/imagecontainer/${type}/${item.id}`}>
+//                 <div className=" rounded-md imgShwd">
+//                   <img
+//                     className="rounded-md cursor-pointer"
+//                     src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+//                     alt="Poster"
+//                   />
+//                 </div>
+//               </NavLink>
+//               <p className="text-sm py-1 capitalize text-gray-300 flex items-center text-slim absolute bottom-[80px] left-[10px]">
+//                 <span>{new Date(item.release_date).getFullYear()}</span>
+
+//                 <LuDot className="text-xl text-gray-300 " />
+//                 <span className="flex items-center gap-x-1 ">
+//                   <MdLocalMovies /> Movie
+//                 </span>
+//               </p>
+//               <h2 className="text-lg font-semibold  max-w-[100%] relative bottom-[30px] left-[10px]">
+//                 {item.title}
+//               </h2>
+//             </div>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
 /* eslint-disable no-unused-vars */
 import { fetchTMDbDataMovies } from "../../api/FetchDataMovie";
 import { MdLocalMovies } from "react-icons/md";
